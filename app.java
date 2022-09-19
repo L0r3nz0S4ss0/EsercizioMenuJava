2ee0bf93eabb39a341f45d1a3d7677a06e484054
Lorenzo Sasso <113933741+L0r3nz0S4ss0@users.noreply.github.com>
2022-09-19 19:49:18 +0200

    Create app.java

---
+++ app.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        boolean isParsed = false;
+        int n = 0;
+        do{
+            //Menu declaration
+            System.out.println("Inserisci un opzione");
+            System.out.println("0: Esci dal programma");
+            System.out.println("1: Stampa hello world");
+            System.out.println("2: Stampa dei numeri da 1 a 10");
+
+            //Scanner instantiation
+            Scanner sc = new Scanner(System.in);
+
+            //Variable Input
+            System.out.println("Inserisci la scelta: ");
+            String s = sc.nextLine();
+
+            //Try parsing the integer
+            try{
+                n = Integer.parseInt(s);
+                isParsed = true;
+            }
+            catch(NumberFormatException nfe){
+                System.out.println("NON E' STATO INSERITO UN VALORE NUMERICO");
+            }
+
+        }while(!isParsed);
+        //Algorithm
+        while(n != 0){
+            //Variable control
+            switch(n){
+                case 0:
+                    System.out.println("Grazie per aver usato il programma");
+                return;
+            
+                case 1:
+                    System.out.println("Hello world");
+                break;
+            
+                case 2:
+                    for(int i = 0; i < 10; i++){
+                        System.out.println(i);
+                    }
+                break;
+            
+                default:
+                    System.out.println("ERRORE, VALORE NON IN RANGE");
+                    return;
+            }
+        }
+    }
+}
